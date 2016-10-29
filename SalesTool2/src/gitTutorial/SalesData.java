c1add7ae24ea750beeddeb968e2714dbfa4b81d9
willellenberg <we00401@georgiasouthern.edu>
2016-10-29 11:26:37 -0400

    Completed Displaying Data in a SalesData Object

--- SalesTool2/src/gitTutorial/SalesData.java
+++ SalesTool2/src/gitTutorial/SalesData.java
@@ -4,8 +4,9 @@ public class SalesData {
    int data[] = {0, 4, 42};
 
    public void display() {
+	   System.out.println("Data: ");
       for(int i = 0 ; i<data.length; i++){
-    	  System.out.println("");
+    	  System.out.println("Next Value: " + data[i]);
       }
    } //End display
 
