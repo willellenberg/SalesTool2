5209892ed4f6b85ba4af6c6cdf1791634c5f4b99
willellenberg <we00401@georgiasouthern.edu>
2016-10-29 10:58:13 -0400

    First

---
+++ SalesTool2/src/gitTutorial/Main.java
@@ -0,0 +1,18 @@
+package gitTutorial;
+
+public class Main {
+
+   public static void main(String[] args) {
+       SalesData data = new SalesData();
+       displayGreeting();
+       data.display();
+   } //End main
+
+
+  private static void displayGreeting() {
+           System.out.println("HELLO HAPPY SALES PEOPLE!");
+           System.out.println("THIS APP SHOWS SALES DATA");
+           System.out.println("Test one");
+  } //End displayGreeting
+
+}  //End class Main
---
+++ SalesTool2/src/gitTutorial/SalesData.java
@@ -0,0 +1,12 @@
+package gitTutorial;
+
+public class SalesData {
+   int data[] = {0, 4, 42};
+
+   public void display() {
+       System.out.println("TODO: Implement data display");
+   } //End display
+
+}  //End class SalesData
+
+  
