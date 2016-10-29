f2a94f4d8bc793d0bc900d1adf2f91293cebaddd
willellenberg <we00401@georgiasouthern.edu>
2016-10-29 11:17:58 -0400

    Partial Implementation of Dispay the Numbers

--- SalesTool2/src/gitTutorial/SalesData.java
+++ SalesTool2/src/gitTutorial/SalesData.java
@@ -4,7 +4,9 @@ public class SalesData {
    int data[] = {0, 4, 42};
 
    public void display() {
-       System.out.println("TODO: Implement data display");
+      for(int i = 0 ; i<data.length; i++){
+    	  System.out.println("");
+      }
    } //End display
 
 }  //End class SalesData
